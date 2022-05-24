042cd11d622df6d609c2cb4bd8bcfa2780f69cea
Marc Delesalle <mrc.delesalle@gmail.com>
2022-05-24 13:00:35 +0200

    pages controller + seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @books = Book.all
+    @users = User.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Book.create(title: Faker::Name.first_name, author: Faker::Name.first_name, address: Faker::Name.first_name, category: Faker::Name.first_name, rating:3, overview:Faker::Name.first_name, review:Faker::Name.first_name, image_url:'https://www.google.com/search?q=chien&sxsrf=ALiCzsbYrGKkxO93RRjB-wKHGpXdEiVS8w:1653388424479&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiOz93t9_f3AhUw4YUKHY9eCloQ_AUoAXoECAIQAw&biw=823&bih=789&dpr=2#imgrc=m3Grp2xHgMbdVM', user_id: 1)
+end
