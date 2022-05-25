f0fd607d0b5efa2fe9e3d482d705108f69dc71c3
Marc Delesalle <mrc.delesalle@gmail.com>
2022-05-25 13:08:41 +0200

    Error management booksAPI

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :author, :address, :price)
+    params.require(:book).permit(:title, :author, :address, :price, :id)
   end
 
   def set_book
@@ -55,10 +55,50 @@ class BooksController < ApplicationController
     parse_serialized = URI.open(url).read
     parse = JSON.parse(parse_serialized)
     {
-      overview: parse["items"][0]["volumeInfo"]["description"],
-      publishing_date: (parse["items"][0]["volumeInfo"]["publishedDate"].length == 7 ? "#{parse['items'][0]['volumeInfo']['publishedDate']}-02" : parse["items"][0]["volumeInfo"]["publishedDate"] ),
-      image_url: (parse["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"].nil? ? "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770" : parse["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]),
-      category: (parse["items"][0]["volumeInfo"]["categories"].nil? ? "N/a" : parse["items"][0]["volumeInfo"]["categories"].first)
+      overview: error_prevention_overview(parse),
+      publishing_date: (parse['items'][0]['volumeInfo']['publishedDate'].length == 7 ? "#{parse['items'][0]['volumeInfo']['publishedDate']}-02" : parse['items'][0]['volumeInfo']['publishedDate']),
+      image_url: error_prevention_image(parse),
+      category: error_prevention_category(parse)
     }
   end
+
+  def error_prevention_overview(parse)
+    begin
+      if parse['items'][0]['volumeInfo']['description'] && (parse['items'][0]['volumeInfo']['description'] != "")
+        return parse['items'][0]['volumeInfo']['description']
+      else
+        return 'An amazing read for everybody!'
+      end
+    rescue StandardError
+      return 'An amazing read for everybody!'
+    end
+  end
+
+  # Refacto avec hash method dig
+
+  def error_prevention_image(parse)
+    begin
+      if parse['items'][0]['volumeInfo']['imageLinks']['thumbnail'] #&& (parse['items'][0]['volumeInfo']['imageLinks']['thumbnail'] != "")
+        return parse['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+      else
+        return 'https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770'
+      end
+    rescue StandardError => e
+      p e
+      return 'https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770'
+    end
+  end
+
+  def error_prevention_category(parse)
+    begin
+      if parse['items'][0]['volumeInfo']['categories'].first && (parse['items'][0]['volumeInfo']['categories'].first != "")
+        return parse['items'][0]['volumeInfo']['categories'].first
+      else
+        return 'Best Sellers'
+      end
+    rescue StandardError
+      return 'Best Sellers'
+    end
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_many :orders
+  has_many :orders, dependent: :destroy
 end
