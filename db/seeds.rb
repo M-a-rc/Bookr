d8a2a6bd5c69067c53cf88debbbbb54c542f24e8
enor de kerdanet <enor2k@gmail.com>
2022-05-25 12:01:45 +0200

    ajout overview

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ n = 0
 5.times do
   user = User.create(username: Faker::Games::Pokemon.name, email: "user#{n += 1}@faker.com", password: "123456")
   rand(5..10).times do
-    book = Book.new(title: Faker::Book.title, author: Faker::Book.author, category: Faker::Book.genre, image_url: "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770", address: Faker::Address.full_address, price: rand(1..2), user_id: user.id)
+    book = Book.new(title: Faker::Book.title, author: Faker::Book.author, category: Faker::Book.genre, image_url: "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770", address: Faker::Address.full_address, price: rand(1..2), overview: Faker::Movie.quote, user_id: user.id)
     book.save
   end
 end
