3ac1e746627269a896365a4539c95c4917eb0b06
enor de kerdanet <enor2k@gmail.com>
2022-05-25 18:32:03 +0200

    validation pour books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
 
+  validates :title, :author, :address, :price, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
