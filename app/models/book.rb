11c1143eb601d7d8534ba814e3c28c29c0388d60
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-05-26 12:59:30 +0200

    location et title OK

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ApplicationRecord
   # Search gem PG
   include PgSearch::Model
   pg_search_scope :search_by_title_and_author,
-    against: [ :title, :author ],
+    against: [:title, :address ],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
